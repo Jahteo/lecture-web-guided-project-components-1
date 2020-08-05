c79128909689c09a53c27ba655b65de7876b3a59
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-05 18:49:58 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -19,15 +19,14 @@ const accordion = document.querySelector('.accordion')
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
 function makePanel(panelObj /* what data does the panel need? */) {
 
-
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
+  const panel = document.createElement('div')
+  const panelBar = document.createElement('div')
+  const panelContent = document.createElement('div')
+  const panelTitle = document.createElement('h3')
+  const panelButtons = document.createElement('div')
+  const openButton = document.createElement('button')
+  const closeButton = document.createElement('button')
 
 
   // TASK 6- Setup the structure of our elements
@@ -61,7 +60,7 @@ function makePanel(panelObj /* what data does the panel need? */) {
   // don't forget to return the panel!
   return null
 }
-
+makePanel({ title: 'foo', content: 'bar' })
 
 // TASK 10- Loop through the panelData in the data folder
 //  creating panels for each content and title,
